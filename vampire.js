b6dbd212a08389992d3284aff819d6c7bd48fbb6
deepthyp1991 <deepthyp1991@gmail.com>
2021-01-12 18:58:28 +0000

    Added more functanalities

--- vampire.js
+++ vampire.js
@@ -10,47 +10,114 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-    this.offspring.push(vampire)
+    this.offspring.push(vampire);
     vampire.creator = this;
-
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
     return this.offspring.length;
-
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
-    et numberOfVampire = 0;
+    let numberOfVampire = 0;
     let currentVampire = this;
 
+    // climb "up" the tree (using iteration), counting nodes, until original vampire is found
     while(currentVampire.creator) {
       currentVampire = currentVampire.creator;
-      numberOfVampire++;
+      numberOfVampire ++;
     }
-    return numberOfVampire
-
+    return numberOfVampire;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-    return this.creator === vampire.creator
-
+    if (this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal) {
+      return true;
+    }
+    else {
+      return false;
+    }
   }
 
-  /** Stretch **/
+  // Returns the vampire object with that name, or null if no vampire exists with that name
+  vampireWithName(name) {
+    if (this.name === name) {
+      return this;
+    }
+    else {
+      return null;
+    }
+  }
 
-  // Returns the closest common ancestor of two vampires.
-  // The closest common anscestor should be the more senior vampire if a direct ancestor is used.
-  // For example:
-  // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
-  // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
-  closestCommonAncestor(vampire) {
+  // Returns the total number of vampires that exist
+  get totalDescendents() {
+    let numberOfVampire = 0;
 
+    numberOfVampire += this.offspring.length;
+
+    for (const descendent of this.offspring) {
+      let lengthOfDescendent = descendent.totalDescendents;
+      numberOfVampire += lengthOfDescendent;
+    }
+    return numberOfVampire;
+  }
+
+  // Returns an array of all the vampires that were converted after 1980
+  get allMillennialVampires() {
+    let millenialVampire = [];
+    if (this.yearConverted >= 2000) {
+      millenialVampire.push(this);
+    }
+
+    for (const descendent of this.offspring) {
+      let millenialDescendent = descendent.allMillennialVampires;
+      millenialVampire.push(millenialDescendent);
+    }
+    return millenialVampire;
+  }
+
+  /** Tree traversal methods **/
+
+  // Returns the vampire object with that name, or null if no vampire exists with that name
+  vampireWithName(name) {
+    if (this.name === name) {
+      return this;
+    }
+    else {
+      return null;
+    }
+  }
+
+  // Returns the total number of vampires that exist
+  get totalDescendents() {
+    let numberOfVampire = 0;
+    numberOfVampire += this.offspring.length;
+
+    for (const descendent of this.offspring) {
+      let numberOfDescendent = descendent.totalDescendents;
+      numberOfVampire += numberOfDescendent;
+    }
+    return numberOfVampire;
+  }
+
+  // Returns an array of all the vampires that were converted after 1980
+  get allMillennialVampires() {
+    let millenialVampire = [];
+    if (this.yearConverted >= 2000) {
+      millenialVampire.push(this);
+    }
+
+    for (const descendent of this.offspring) {
+      let millenialDescendent = descendent.allMillennialVampires;
+      millenialVampire.push(millenialDescendent)
+    }
+    return millenialVampire;
   }
 }
 
-module.exports = Vampire;
 
+
+module.exports = Vampire;
